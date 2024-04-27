3125391e4ba28bd2064a104592fb8971a0f41c03
kth <rnjsxo1017@gmail.com>
2024-04-27 23:52:04 +0900

    wrapper

--- src/lang/wrapper/WrapperClassMain.java
+++ src/lang/wrapper/WrapperClassMain.java
@@ -3,21 +3,15 @@ package lang.wrapper;
 public class WrapperClassMain {
 
     public static void main(String[] args) {
-        Integer newInteger = new Integer(10); //미래에 삭제 예정, 대신에 valueOf()를 사용
-        Integer integerObj = Integer.valueOf(10); //-128 ~ 127 자주 사용하는 숫자 값 재사용, 불변
-        Long longObj = Long.valueOf(100);
-        Double doubleObj = Double.valueOf(10.5);
+        Integer newInteger = new Integer(10); //9이후 deprecated, 대신에 valueOf()를 사용
+        Integer integerObj = Integer.valueOf(10); //-128 ~ 127 자주 사용하는 숫자 값 캐싱, 재사용, 불변
 
         System.out.println("newInteger = " + newInteger);
         System.out.println("integerObj = " + integerObj);
-        System.out.println("longObj = " + longObj);
-        System.out.println("doubleObj = " + doubleObj);
 
         System.out.println("내부 값 읽기");
         int intValue = integerObj.intValue();
         System.out.println("intValue = " + intValue);
-        long longValue = longObj.longValue();
-        System.out.println("longValue = " + longValue);
 
         System.out.println("비교");
         System.out.println("==: " + (newInteger == integerObj));
