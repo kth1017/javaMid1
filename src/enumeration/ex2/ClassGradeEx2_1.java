ae943fcd7e1049c0c3e708f293738d99b4ee1157
KTH <rnjsxo1017@gmail.com>
2024-04-29 18:57:54 +0900

    240429

--- src/enumeration/ex2/ClassGradeEx2_1.java
+++ src/enumeration/ex2/ClassGradeEx2_1.java
@@ -5,6 +5,7 @@ public class ClassGradeEx2_1 {
     public static void main(String[] args) {
         int price = 10000;
 
+
         DiscountService discountService = new DiscountService();
         int basic = discountService.discount(ClassGrade.BASIC, price);
         int gold = discountService.discount(ClassGrade.GOLD, price);
--- src/enumeration/ex2/DiscountService.java
+++ src/enumeration/ex2/DiscountService.java
@@ -5,6 +5,7 @@ public class DiscountService {
     public int discount(ClassGrade classGrade, int price) {
         int discountPercent = 0;
 
+        // 어차피 인스턴스가 정해져 ==을 써도 무방
         if (classGrade == ClassGrade.BASIC) {
             discountPercent = 10;
         } else if (classGrade == ClassGrade.GOLD) {
