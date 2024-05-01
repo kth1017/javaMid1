f66e7d6071768d4d7f6e305dbf6893ac8382ec3b
kth <rnjsxo1017@gmail.com>
2024-05-01 22:28:53 +0900

    0501

--- src/time/GetTimeMain.java
+++ src/time/GetTimeMain.java
@@ -6,6 +6,7 @@ import java.time.temporal.ChronoField;
 public class GetTimeMain {
 
     public static void main(String[] args) {
+        // chronoField 사용
         LocalDateTime dt = LocalDateTime.of(2030, 1, 1, 13, 30, 59);
         System.out.println("YEAR = " + dt.get(ChronoField.YEAR));
         System.out.println("MONTH_OF_YEAR = " + dt.get(ChronoField.MONTH_OF_YEAR));
