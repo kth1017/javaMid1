87207f5a1393e24b102d878081fc0ee4abf91101
kth <rnjsxo1017@gmail.com>
2024-05-06 21:09:27 +0900

    0506

--- src/time/IsSupportedMain2.java
+++ src/time/IsSupportedMain2.java
@@ -6,6 +6,13 @@ import java.time.temporal.ChronoField;
 public class IsSupportedMain2 {
 
     public static void main(String[] args) {
+        /*// time으로 모든 시각을 만능으로 조회할 순 없음
+        public static void main(String[] args) {
+            // 분 없는 시각
+            LocalDate now = LocalDate.now();
+            int minute = now.get(ChronoField.SECOND_OF_MINUTE);
+            System.out.println("minute = " + minute);*/
+        
         LocalDate now = LocalDate.now();
 
         boolean supported = now.isSupported(ChronoField.SECOND_OF_MINUTE);
