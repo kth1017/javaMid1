d9e342a97b6874ec520003c5dfb7e2d5ffdd3fe9
kth <rnjsxo1017@gmail.com>
2024-05-06 21:16:26 +0900

    ~time

--- src/time/FormattingMain2.java
+++ src/time/FormattingMain2.java
@@ -6,8 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class FormattingMain2 {
 
     public static void main(String[] args) {
-        // 포맷팅: 날짜와 시간을 문자로
+        // 포맷팅: 날짜와 시간을 문자로(기존 포매팅 문자를 사용할 필요없음)
         LocalDateTime now = LocalDateTime.of(2024, 12, 31, 13, 30, 59);
+        // M과 m 주의
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedDateTime = now.format(formatter);
         System.out.println("날짜와 시간 포맷팅: " + formattedDateTime);
